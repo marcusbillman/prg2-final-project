9180f6a75f94e3b1e581d9297a24db26ae45c85e
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-02 21:18:19 +0200

    ✨ ServerNetworkModel: Implement sending parcels

--- src/ServerNetworkModel.java
+++ src/ServerNetworkModel.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,6 +8,7 @@ public class ServerNetworkModel {
 
     private ServerSocket serverSocket;
     private Socket socket;
+    private ObjectOutputStream out;
 
     public ServerNetworkModel(int port) {
         this.port = port;
@@ -16,9 +18,20 @@ public class ServerNetworkModel {
         try {
             serverSocket = new ServerSocket(port);
             socket = serverSocket.accept();
+            out = new ObjectOutputStream(socket.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return socket;
+    }
+
+    public void sendParcel(String feature, Object payload) {
+        Parcel parcel = new Parcel("request", feature, payload);
+        try {
+            out.writeObject(parcel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
