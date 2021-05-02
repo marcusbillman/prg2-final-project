e583ed66883813fe272cb167d574405f4e772d76
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-02 20:31:11 +0200

    ✨ ClientController: Ask for server address

--- src/ClientController.java
+++ src/ClientController.java
@@ -1,11 +1,13 @@
+import javax.swing.*;
+
 public class ClientController {
     private ClientNetworkModel clientNetworkModel;
 
     public ClientController(ClientNetworkModel clientNetworkModel) {
         this.clientNetworkModel = clientNetworkModel;
 
-        this.clientNetworkModel.connect("localhost", 1337);
-
+        String serverAddress = JOptionPane.showInputDialog("Server address", "localhost");
+        this.clientNetworkModel.connect(serverAddress, 1337);
         System.out.println("Connected to server");
     }
 }
