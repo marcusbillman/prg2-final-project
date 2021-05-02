8b81c5bd395a4ea585ffc204e9017543d678163a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-02 21:28:02 +0200

    ✨ ClientController: Continuously receive parcels

--- src/ClientController.java
+++ src/ClientController.java
@@ -9,5 +9,14 @@ public class ClientController {
         String serverAddress = JOptionPane.showInputDialog("Server address", "localhost");
         this.clientNetworkModel.connect(serverAddress, 1337);
         System.out.println("Connected to server");
+
+        startReceiving();
+    }
+
+    private void startReceiving() {
+        while (clientNetworkModel.getSocket().isConnected()) {
+            Parcel parcel = this.clientNetworkModel.receiveParcel();
+            System.out.println(parcel.getType() + " | " + parcel.getFeature() + " | " + parcel.getPayload());
+        }
     }
 }
