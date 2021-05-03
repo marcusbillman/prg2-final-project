8698edf6e7a215db167eebad8ccf0c141f17958a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-03 19:34:54 +0200

    💄 ServerView: UI for popup feature

--- src/ServerView.java
+++ src/ServerView.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class ServerView {
     private final JFrame frame;
@@ -14,6 +15,9 @@ public class ServerView {
     private JPanel terminalTab;
     private JPanel filesTab;
     private JPanel screenTab;
+    private JTextArea popupBodyTextArea;
+    private JButton popupSendButton;
+    private JTextField popupTitleTextField;
 
     public ServerView() {
         frame = new JFrame("Server");
@@ -22,11 +26,25 @@ public class ServerView {
         frame.setVisible(true);
     }
 
+    public String getPopupTitle() {
+        return popupTitleTextField.getText();
+    }
+
+    public String getPopupBody() {
+        return popupBodyTextArea.getText();
+    }
+
     public void setProgressBarVisible(boolean isVisible) {
         progressBar.setVisible(isVisible);
     }
 
     public void setStatusLabelText(String text) {
         statusLabel.setText(text);
+    }
+
+    // -------------------- Listeners
+
+    public void addPopupSendButtonListener(ActionListener actionListener) {
+        popupSendButton.addActionListener(actionListener);
     }
 }
