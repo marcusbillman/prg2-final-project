46d7d44c2c95b0076bffcb2a05b1704559105366
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-03 10:45:43 +0200

    💄 ServerView: GUI skeleton with status setters

--- src/ServerView.java
+++ src/ServerView.java
@@ -4,10 +4,29 @@ public class ServerView {
     private final JFrame frame;
     private JPanel mainPanel;
 
+    private JPanel statusPanel;
+    private JProgressBar progressBar;
+    private JLabel statusLabel;
+
+    private JTabbedPane tabbedPane;
+    private JPanel popupTab;
+    private JPanel applicationTab;
+    private JPanel terminalTab;
+    private JPanel filesTab;
+    private JPanel screenTab;
+
     public ServerView() {
         frame = new JFrame("Server");
         frame.setContentPane(mainPanel);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void setProgressBarVisible(boolean isVisible) {
+        progressBar.setVisible(isVisible);
+    }
+
+    public void setStatusLabelText(String text) {
+        statusLabel.setText(text);
     }
 }
