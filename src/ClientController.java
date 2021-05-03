c8c0b35cd4cc96eab8bb06fc2baabb9bfa8447bf
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-05-03 19:41:07 +0200

    ✨ ClientController: Handle receiving popup request

--- src/ClientController.java
+++ src/ClientController.java
@@ -16,7 +16,26 @@ public class ClientController {
     private void startReceiving() {
         while (clientNetworkModel.getSocket().isConnected()) {
             Parcel parcel = this.clientNetworkModel.receiveParcel();
-            System.out.println(parcel.getType() + " | " + parcel.getFeature() + " | " + parcel.getPayload());
+
+            String type = parcel.getType();
+            String feature = parcel.getFeature();
+            Object payload = parcel.getPayload();
+
+            System.out.println(type + " | " + feature + " | " + payload);
+
+            switch (feature) {
+                case "popup":
+                    handlePopupParcel(payload);
+                    break;
+                default:
+                    throw new IllegalArgumentException("'" + feature + "' is not a valid feature name");
+            }
         }
+    }
+
+    private void handlePopupParcel(Object payload) {
+        String[] payloadArray = (String[]) payload;
+        JOptionPane.showMessageDialog(
+                null, payloadArray[0], payloadArray[1], JOptionPane.INFORMATION_MESSAGE);
     }
 }
